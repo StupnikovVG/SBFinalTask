94c0c34fda0645874a79f311503b99aacc8fd43d
Stupnikov.VG <Stupnikov.VG@gmail.com>
2017-05-23 14:33:49 +0500

    Добавил автозапуск

---
+++ features/Run.bat
@@ -0,0 +1,2 @@
+"Z:\Program Files (x86)\1cv8\8.3.10.2168\bin\1cv8.exe" /EXECUTE Z:\Repo\vanessa-behavior\vanessa-behavior.epf /C"StartFeaturePlayer;VBParams=Z:\Repo\SBFinalTask\features\autorun.json"
+pause
